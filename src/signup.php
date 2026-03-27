6ff6b9246402219be05872fe1aee1da95e7235b2
Sebas Maya <msmadronero.6346@unicesmag.edu.co>
2026-03-27 12:15:20 -0500

    feature 3 register user backend

--- src/signup.php
+++ src/signup.php
@@ -27,12 +27,24 @@
     }
 
     //telefono 
-    $check_phone = "SELECT mobile_phone FROM users_model WHERE mobile_phone = '$m_phone'";
+    $check_phone = "SELECT mobile_phone FROM users_model WHERE mobile_phone = '$phone'";
     $res_phone = pg_query($local_conn, $check_phone);
 
     if (pg_num_rows($res_phone) > 0) {
-        echo "Error: El número de celular '$m_phone' ya está registrado en nuestro sistema."; 
+        echo "Error: El número de celular '$phone' ya está registrado en nuestro sistema."; 
         exit();
+    }
+
+    if ($res_local) {
+    $res_supa = pg_query($supa_conn, $sql);
+
+    if ($res_supa) {
+        echo "¡Listo! Guardado en ambos lados.";
+    } else {
+        echo "Error: Se guardó en local pero no en la nube.";
+    }
+    } else {
+    echo "Error: No se pudo guardar ni en local ni en supa";
     }
 
     $res_local = pg_query($local_conn, $sql); 
