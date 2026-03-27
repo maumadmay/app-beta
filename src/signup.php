84fd872fa51baa7c4d295e98012f2c37214de56c
Sebas Maya <msmadronero.6346@unicesmag.edu.co>
2026-03-27 12:05:45 -0500

    feature 1, email comprobation

--- src/signup.php
+++ src/signup.php
@@ -18,5 +18,17 @@
     VALUES ('$f_name', '$l_name', '$mail', '$phone', '$enc_pass')";
 
     //execute query
-    pg_query($sql);
+
+    //rama 1
+    $check_email = "SELECT email FROM users WHERE email = '$mail'";
+    $res_email = pg_query($local_conn, $check_email);
+
+    if (pg_num_rows($res_email) > 0) {
+        echo "Error: El correo electrónico '$mail' ya está registrado. Por favor, use uno diferente.\n";
+        exit();
+    }
+
+    $res_local = pg_query($local_conn, $sql); 
+    
+    //pg_query($sql);
 ?>
